4946e573a5c0f573538cdd330d25316e49406298
kaorishi11 <kaorishimada11@gmail.com>
2026-05-08 14:26:51 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -64,32 +64,27 @@ class _HomePageState extends State<HomePage> {
     {
       "nome": "Como eu era antes de você",
       "imagem": "../images/filme1.jpg",
-      "descricao":
-      "Um romance emocionante sobre amor, escolhas e superação.",
+      "descricao": "É um drama romântico baseado no livro de Jojo Moyes, focado na transformação da vida de Will Traynor, um homem rico, esportista e ativo que se torna tetraplégico após um acidente. Depressivo e cínico, ele contrata Louisa Clark, uma jovem excêntrica e modesta, como cuidadora, mudando a vida de ambos. ",
     },
     {
       "nome": "O badboy e eu",
       "imagem": "../images/filme2.jpg",
-      "descricao":
-      "Uma história de amor entre um badboy rebelde e uma garota doce.",
+      "descricao": "Um romance adolescente que segue a história de uma jovem que se apaixona por um badboy, enfrentando desafios e descobrindo o poder do amor verdadeiro. É um filme de romance adolescente baseado em uma história de sucesso do Wattpad, focado na dinâmica entre uma dançarina focada e um jogador de futebol americano popular. ",
     },
     {
       "nome": "Wicked",
       "imagem": "../images/filme3.jpg",
-      "descricao":
-      "Um musical mágico que mostra a verdadeira história das bruxas de Oz.",
+      "descricao": "É um renomado musical da Broadway e, mais recentemente, uma superprodução cinematográfica dividida em duas partes, que funciona como um prelúdio e releitura de O Mágico de Oz. A história explora a amizade improvável entre duas bruxas e revela a verdadeira origem da Bruxa Má do Oeste, mudando a perspectiva clássica da história.",
     },
     {
       "nome": "Viva a vida é uma festa",
       "imagem": "../images/filme4.jpg",
-      "descricao":
-      "Uma aventura musical emocionante sobre família e tradições.",
+      "descricao": "É uma animação da Pixar que retrata a jornada de Miguel, um menino de 12 anos que sonha ser músico, mas sua família proíbe música. No Día de Muertos, ele é transportado para a vibrante e colorida Terra dos Mortos, onde busca seu ídolo e descobre segredos familiares sobre amor, memória e ancestralidade. ",
     },
     {
       "nome": "A empregada",
       "imagem": "../images/filme5.jpg",
-      "descricao":
-      "Um drama intenso sobre desigualdade, justiça e sobrevivência.",
+      "descricao": "É um thriller psicológico de ritmo acelerado que narra a história de Millie, uma ex-presidiária que aceita emprego como empregada doméstica na mansão de Nina e Andrew Winchester, envolvendo-se em segredos obscuros, manipulação e reviravoltas perigosas. ",
     },
   ];
 
